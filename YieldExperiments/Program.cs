ac2517486dcf39bdf9b8e21f4614eb52c92a7fa5
Romualdo Villalobos <romualdo.villalobos@wizard.com.co>
2021-11-28 17:17:09 -0500

    Added experiments for yields inside while

--- YieldExperiments/Program.cs
+++ YieldExperiments/Program.cs
@@ -16,12 +16,19 @@ namespace YieldExperiments
             private int someFieldName = 0;
         }
 
+        int somePrivateField = 22;
+        static int someStaticField = 23;
+
         static void Main(string[] args)
         {
             Playground01();
             Playground02();
             Playground03();
             Playground04();
+            Playground05();
+
+            var program = new Program();
+            program.Playground06();
         }
 
         static void Playground01()
@@ -132,18 +139,103 @@ namespace YieldExperiments
             Console.WriteLine(enumerator.Current);
             enumerator.MoveNext();
             Console.WriteLine(enumerator.Current);
+
+            // Yield inside while
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
         }
 
         static IEnumerator<MyCustomClass> HelloEnumeratorWithCustomClass()
         {
             yield return new MyCustomClass() { SomeFieldName = 5 };
 
-            var obj = new MyCustomClass();
-            obj.SomeFieldName = 3;
-            yield return obj;
+            var myCustomObj = new MyCustomClass();
+            myCustomObj.SomeFieldName = 3;
+            yield return myCustomObj;
 
-            obj.SomeFieldName = -7;
-            yield return obj;
+            myCustomObj.SomeFieldName = -3;
+            yield return myCustomObj;
+
+            int i = myCustomObj.SomeFieldName;
+            while (i != 0)
+            {
+                ++i;
+                yield return null;
+            }
+        }
+
+        static void Playground05()
+        {
+            Console.WriteLine("Playground02");
+            var anEnumeratorObject = HelloEnumeratorWhileTrue();
+
+            // Iterate will produce error
+            //foreach (var item in aEnumeratorObject)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // Use enumerator and movenext
+            var enumerator = anEnumeratorObject;
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+        }
+
+        static IEnumerator<int> HelloEnumeratorWhileTrue()
+        {
+            yield return 5;
+
+            int i = 0;
+            while (i != 2)
+            {
+                yield return i++;
+            }
+
+            someStaticField = 25;
+        }
+
+        void Playground06()
+        {
+            Console.WriteLine("Playground02");
+            var anEnumeratorObject = HelloEnumeratorAccessPrivateMember();
+
+            // Iterate will produce error
+            //foreach (var item in aEnumeratorObject)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // Use enumerator and movenext
+            var enumerator = anEnumeratorObject;
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+            enumerator.MoveNext();
+            Console.WriteLine(enumerator.Current);
+        }
+
+        IEnumerator<int> HelloEnumeratorAccessPrivateMember()
+        {
+            yield return 5;
+
+            int i = 0;
+            while (i != 2)
+            {
+                yield return i++;
+            }
+
+            somePrivateField = 25;
         }
     }
 }
--- disassembled/src/YieldExperiments/Program.cs
+++ disassembled/src/YieldExperiments/Program.cs
@@ -8,21 +8,36 @@
 
     internal class Program
     {
-        [IteratorStateMachine((Type) typeof(<HelloEnumerable>d__3))]
+        private int somePrivateField = 0x16;
+        private static int someStaticField = 0x17;
+
+        [IteratorStateMachine((Type) typeof(<HelloEnumerable>d__5))]
         private static IEnumerable<int> HelloEnumerable() => 
-            new <HelloEnumerable>d__3(-2);
+            new <HelloEnumerable>d__5(-2);
 
-        [IteratorStateMachine((Type) typeof(<HelloEnumerableWithState>d__7))]
+        [IteratorStateMachine((Type) typeof(<HelloEnumerableWithState>d__9))]
         private static IEnumerable<int> HelloEnumerableWithState() => 
-            new <HelloEnumerableWithState>d__7(-2);
+            new <HelloEnumerableWithState>d__9(-2);
 
-        [IteratorStateMachine((Type) typeof(<HelloEnumerator>d__5))]
+        [IteratorStateMachine((Type) typeof(<HelloEnumerator>d__7))]
         private static IEnumerator<int> HelloEnumerator() => 
-            new <HelloEnumerator>d__5(0);
+            new <HelloEnumerator>d__7(0);
 
-        [IteratorStateMachine((Type) typeof(<HelloEnumeratorWithCustomClass>d__9))]
+        [IteratorStateMachine((Type) typeof(<HelloEnumeratorAccessPrivateMember>d__15))]
+        private IEnumerator<int> HelloEnumeratorAccessPrivateMember()
+        {
+            <HelloEnumeratorAccessPrivateMember>d__15 d__1 = new <HelloEnumeratorAccessPrivateMember>d__15(0);
+            d__1.<>4__this = this;
+            return d__1;
+        }
+
+        [IteratorStateMachine((Type) typeof(<HelloEnumeratorWhileTrue>d__13))]
+        private static IEnumerator<int> HelloEnumeratorWhileTrue() => 
+            new <HelloEnumeratorWhileTrue>d__13(0);
+
+        [IteratorStateMachine((Type) typeof(<HelloEnumeratorWithCustomClass>d__11))]
         private static IEnumerator<MyCustomClass> HelloEnumeratorWithCustomClass() => 
-            new <HelloEnumeratorWithCustomClass>d__9(0);
+            new <HelloEnumeratorWithCustomClass>d__11(0);
 
         private static void Main(string[] args)
         {
@@ -30,6 +45,8 @@
             Playground02();
             Playground03();
             Playground04();
+            Playground05();
+            new Program().Playground06();
         }
 
         private static void Playground01()
@@ -88,17 +105,49 @@
             Console.WriteLine(enumerator2.Current);
             enumerator2.MoveNext();
             Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+        }
+
+        private static void Playground05()
+        {
+            Console.WriteLine("Playground02");
+            IEnumerator<int> enumerator2 = HelloEnumeratorWhileTrue();
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+        }
+
+        private void Playground06()
+        {
+            Console.WriteLine("Playground02");
+            IEnumerator<int> enumerator2 = this.HelloEnumeratorAccessPrivateMember();
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
+            enumerator2.MoveNext();
+            Console.WriteLine(enumerator2.Current);
         }
 
         [CompilerGenerated]
-        private sealed class <HelloEnumerable>d__3 : IEnumerable<int>, IEnumerable, IEnumerator<int>, IEnumerator, IDisposable
+        private sealed class <HelloEnumerable>d__5 : IEnumerable<int>, IEnumerable, IEnumerator<int>, IEnumerator, IDisposable
         {
             private int <>1__state;
             private int <>2__current;
             private int <>l__initialThreadId;
 
             [DebuggerHidden]
-            public <HelloEnumerable>d__3(int <>1__state)
+            public <HelloEnumerable>d__5(int <>1__state)
             {
                 this.<>1__state = <>1__state;
                 this.<>l__initialThreadId = Environment.CurrentManagedThreadId;
@@ -136,10 +185,10 @@
             [DebuggerHidden]
             IEnumerator<int> IEnumerable<int>.GetEnumerator()
             {
-                Program.<HelloEnumerable>d__3 d__;
+                Program.<HelloEnumerable>d__5 d__;
                 if ((this.<>1__state != -2) || (this.<>l__initialThreadId != Environment.CurrentManagedThreadId))
                 {
-                    d__ = new Program.<HelloEnumerable>d__3(0);
+                    d__ = new Program.<HelloEnumerable>d__5(0);
                 }
                 else
                 {
@@ -172,7 +221,7 @@
         }
 
         [CompilerGenerated]
-        private sealed class <HelloEnumerableWithState>d__7 : IEnumerable<int>, IEnumerable, IEnumerator<int>, IEnumerator, IDisposable
+        private sealed class <HelloEnumerableWithState>d__9 : IEnumerable<int>, IEnumerable, IEnumerator<int>, IEnumerator, IDisposable
         {
             private int <>1__state;
             private int <>2__current;
@@ -180,7 +229,7 @@
             private int <fooVariable>5__1;
 
             [DebuggerHidden]
-            public <HelloEnumerableWithState>d__7(int <>1__state)
+            public <HelloEnumerableWithState>d__9(int <>1__state)
             {
                 this.<>1__state = <>1__state;
                 this.<>l__initialThreadId = Environment.CurrentManagedThreadId;
@@ -224,10 +273,10 @@
             [DebuggerHidden]
             IEnumerator<int> IEnumerable<int>.GetEnumerator()
             {
-                Program.<HelloEnumerableWithState>d__7 d__;
+                Program.<HelloEnumerableWithState>d__9 d__;
                 if ((this.<>1__state != -2) || (this.<>l__initialThreadId != Environment.CurrentManagedThreadId))
                 {
-                    d__ = new Program.<HelloEnumerableWithState>d__7(0);
+                    d__ = new Program.<HelloEnumerableWithState>d__9(0);
                 }
                 else
                 {
@@ -260,13 +309,13 @@
         }
 
         [CompilerGenerated]
-        private sealed class <HelloEnumerator>d__5 : IEnumerator<int>, IEnumerator, IDisposable
+        private sealed class <HelloEnumerator>d__7 : IEnumerator<int>, IEnumerator, IDisposable
         {
             private int <>1__state;
             private int <>2__current;
 
             [DebuggerHidden]
-            public <HelloEnumerator>d__5(int <>1__state)
+            public <HelloEnumerator>d__7(int <>1__state)
             {
                 this.<>1__state = <>1__state;
             }
@@ -319,14 +368,146 @@
         }
 
         [CompilerGenerated]
-        private sealed class <HelloEnumeratorWithCustomClass>d__9 : IEnumerator<Program.MyCustomClass>, IEnumerator, IDisposable
+        private sealed class <HelloEnumeratorAccessPrivateMember>d__15 : IEnumerator<int>, IEnumerator, IDisposable
+        {
+            private int <>1__state;
+            private int <>2__current;
+            public Program <>4__this;
+            private int <i>5__1;
+
+            [DebuggerHidden]
+            public <HelloEnumeratorAccessPrivateMember>d__15(int <>1__state)
+            {
+                this.<>1__state = <>1__state;
+            }
+
+            private bool MoveNext()
+            {
+                switch (this.<>1__state)
+                {
+                    case 0:
+                        this.<>1__state = -1;
+                        this.<>2__current = 5;
+                        this.<>1__state = 1;
+                        return true;
+
+                    case 1:
+                        this.<>1__state = -1;
+                        this.<i>5__1 = 0;
+                        break;
+
+                    case 2:
+                        this.<>1__state = -1;
+                        break;
+
+                    default:
+                        return false;
+                }
+                if (this.<i>5__1 == 2)
+                {
+                    this.<>4__this.somePrivateField = 0x19;
+                    return false;
+                }
+                int num2 = this.<i>5__1;
+                this.<i>5__1 = num2 + 1;
+                this.<>2__current = num2;
+                this.<>1__state = 2;
+                return true;
+            }
+
+            [DebuggerHidden]
+            void IEnumerator.Reset()
+            {
+                throw new NotSupportedException();
+            }
+
+            [DebuggerHidden]
+            void IDisposable.Dispose()
+            {
+            }
+
+            int IEnumerator<int>.Current =>
+                this.<>2__current;
+
+            object IEnumerator.Current =>
+                (int) this.<>2__current;
+        }
+
+        [CompilerGenerated]
+        private sealed class <HelloEnumeratorWhileTrue>d__13 : IEnumerator<int>, IEnumerator, IDisposable
+        {
+            private int <>1__state;
+            private int <>2__current;
+            private int <i>5__1;
+
+            [DebuggerHidden]
+            public <HelloEnumeratorWhileTrue>d__13(int <>1__state)
+            {
+                this.<>1__state = <>1__state;
+            }
+
+            private bool MoveNext()
+            {
+                switch (this.<>1__state)
+                {
+                    case 0:
+                        this.<>1__state = -1;
+                        this.<>2__current = 5;
+                        this.<>1__state = 1;
+                        return true;
+
+                    case 1:
+                        this.<>1__state = -1;
+                        this.<i>5__1 = 0;
+                        break;
+
+                    case 2:
+                        this.<>1__state = -1;
+                        break;
+
+                    default:
+                        return false;
+                }
+                if (this.<i>5__1 == 2)
+                {
+                    Program.someStaticField = 0x19;
+                    return false;
+                }
+                int num2 = this.<i>5__1;
+                this.<i>5__1 = num2 + 1;
+                this.<>2__current = num2;
+                this.<>1__state = 2;
+                return true;
+            }
+
+            [DebuggerHidden]
+            void IEnumerator.Reset()
+            {
+                throw new NotSupportedException();
+            }
+
+            [DebuggerHidden]
+            void IDisposable.Dispose()
+            {
+            }
+
+            int IEnumerator<int>.Current =>
+                this.<>2__current;
+
+            object IEnumerator.Current =>
+                (int) this.<>2__current;
+        }
+
+        [CompilerGenerated]
+        private sealed class <HelloEnumeratorWithCustomClass>d__11 : IEnumerator<Program.MyCustomClass>, IEnumerator, IDisposable
         {
             private int <>1__state;
             private Program.MyCustomClass <>2__current;
-            private Program.MyCustomClass <obj>5__1;
+            private Program.MyCustomClass <myCustomObj>5__1;
+            private int <i>5__2;
 
             [DebuggerHidden]
-            public <HelloEnumeratorWithCustomClass>d__9(int <>1__state)
+            public <HelloEnumeratorWithCustomClass>d__11(int <>1__state)
             {
                 this.<>1__state = <>1__state;
             }
@@ -346,24 +527,40 @@
                     }
                     case 1:
                         this.<>1__state = -1;
-                        this.<obj>5__1 = new Program.MyCustomClass();
-                        this.<obj>5__1.SomeFieldName = 3;
-                        this.<>2__current = this.<obj>5__1;
+                        this.<myCustomObj>5__1 = new Program.MyCustomClass();
+                        this.<myCustomObj>5__1.SomeFieldName = 3;
+                        this.<>2__current = this.<myCustomObj>5__1;
                         this.<>1__state = 2;
                         return true;
 
                     case 2:
                         this.<>1__state = -1;
-                        this.<obj>5__1.SomeFieldName = -7;
-                        this.<>2__current = this.<obj>5__1;
+                        this.<myCustomObj>5__1.SomeFieldName = -3;
+                        this.<>2__current = this.<myCustomObj>5__1;
                         this.<>1__state = 3;
                         return true;
 
                     case 3:
                         this.<>1__state = -1;
+                        this.<i>5__2 = this.<myCustomObj>5__1.SomeFieldName;
+                        break;
+
+                    case 4:
+                        this.<>1__state = -1;
+                        break;
+
+                    default:
                         return false;
                 }
-                return false;
+                if (this.<i>5__2 == 0)
+                {
+                    return false;
+                }
+                int num2 = this.<i>5__2 + 1;
+                this.<i>5__2 = num2;
+                this.<>2__current = null;
+                this.<>1__state = 4;
+                return true;
             }
 
             [DebuggerHidden]
